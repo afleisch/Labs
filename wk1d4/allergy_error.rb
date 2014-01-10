f552395e419fccf6982deef7bdd027ce9db56987
afleisch <afleisch@gmail.com>
2014-01-09 23:27:05 -0800

    wk1d4 lab files

---
+++ wk1d4/allergy_error.rb
@@ -0,0 +1,5 @@
+class AllergyError < ArgumentError; end
+# to create a new class of ArgumentError that 
+#will inherit the functions of the ArgumentErrorclass
+#but is specific to my program and that has been 
+#customized for my purposes
